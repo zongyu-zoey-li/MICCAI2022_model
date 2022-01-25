b547fe2bd43caa19fd3bc4f7c0c0d044ac38dc90
zongyu-zoey-li <78986418+zongyu-zoey-li@users.noreply.github.com>
2022-01-25 15:19:58 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ from __future__ import print_function
 import json
 import pdb
 import math
-
 all_params = json.load(open('config.json'))
-sample_rate = all_params["experiment_setup"]["sample_rate"]
-tcn_model_params = all_params["JIGSAWS"]["tcn_params"]
-input_size = all_params["JIGSAWS"]["input_size"]
-num_class=all_params["JIGSAWS"]["gesture_class_num"]
 dataset_name = all_params['dataset_name']
-raw_feature_dir = all_params["JIGSAWS"]["raw_feature_dir"]
-test_trial=all_params["JIGSAWS"]["test_trial"]
-train_trial = all_params["JIGSAWS"]["train_trial"]
-gesture_class_num = all_params["JIGSAWS"]["gesture_class_num"]
+tcn_model_params = all_params[dataset_name]["tcn_params"]
+input_size = all_params[dataset_name]["input_size"]
+num_class=all_params[dataset_name]["gesture_class_num"]
+
+raw_feature_dir = all_params[dataset_name]["raw_feature_dir"]
+test_trial=all_params[dataset_name]["test_trial"]
+train_trial = all_params[dataset_name]["train_trial"]
+sample_rate = all_params[dataset_name]["sample_rate"]
+gesture_class_num = all_params[dataset_name]["gesture_class_num"]
 # for parameter tuning
-validation_trial = all_params["JIGSAWS"]["validation_trial"]
-validation_trial_train = [2,3,4,5]
+validation_trial = all_params[dataset_name]["validation_trial"]
+validation_trial_train = [2,3,4,5,6]
