5a54cf68517a09373c3e68383b1aa1380771236e
zongyu-zoey-li <78986418+zongyu-zoey-li@users.noreply.github.com>
2022-01-25 15:19:04 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ import pdb
 import math
 
 all_params = json.load(open('config.json'))
-sample_rate = all_params["experiment_setup"]['sample_rate']
-tcn_model_dir=all_params["JIGSAWS"]['tcn_model_dir']
+sample_rate = all_params["experiment_setup"]["sample_rate"]
 tcn_model_params = all_params["JIGSAWS"]["tcn_params"]
 input_size = all_params["JIGSAWS"]["input_size"]
 num_class=all_params["JIGSAWS"]["gesture_class_num"]
@@ -15,7 +14,6 @@ dataset_name = all_params['dataset_name']
 raw_feature_dir = all_params["JIGSAWS"]["raw_feature_dir"]
 test_trial=all_params["JIGSAWS"]["test_trial"]
 train_trial = all_params["JIGSAWS"]["train_trial"]
-sample_rate = all_params["experiment_setup"]["sample_rate"]
 gesture_class_num = all_params["JIGSAWS"]["gesture_class_num"]
 # for parameter tuning
 validation_trial = all_params["JIGSAWS"]["validation_trial"]
