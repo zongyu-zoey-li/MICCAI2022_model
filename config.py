062964831cb2e1a494191ab742f8a8ea4d6e2941
zongyu-zoey-li <78986418+zongyu-zoey-li@users.noreply.github.com>
2022-01-27 15:55:05 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import json
 import pdb
 import math
+
 all_params = json.load(open('config.json'))
 dataset_name = all_params['dataset_name']
 tcn_model_params = all_params[dataset_name]["tcn_params"]
@@ -15,6 +16,8 @@ test_trial=all_params[dataset_name]["test_trial"]
 train_trial = all_params[dataset_name]["train_trial"]
 sample_rate = all_params[dataset_name]["sample_rate"]
 gesture_class_num = all_params[dataset_name]["gesture_class_num"]
+data_transform_path = all_params[dataset_name]["data_transform_path"]
 # for parameter tuning
 validation_trial = all_params[dataset_name]["validation_trial"]
-validation_trial_train = [2,3,4,5,6]
+validation_trial_train = #[2,3,4,5,6] "Desk" or [2,3,4,5]"JIGSAWS"
+LOCS =all_params[dataset_name]["locs"]
